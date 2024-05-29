b4d1861051a16132502de24d5632c5d0715b0b2f
Drillllll <118617644+Drillllll@users.noreply.github.com>
2024-05-29 11:13:37 +0200

    init

--- src/main/java/internship/java/java_internship/JavaInternshipApplication.java
+++ src/main/java/internship/java/java_internship/JavaInternshipApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaInternshipApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaInternshipApplication.class, args);
+		SpringApplication.run(JavaInternshipApplication.class,  args);
 	}
 
 }
